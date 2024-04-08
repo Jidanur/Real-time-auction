6c8483516aa9fb7ebaf8aabccd29cd4f3fcab48d
jidan <jidanurrahman@ymail.com>
2024-04-08 08:20:01 -0500

    added userlogin validation

--- backend/kafka/kafka-streams-auction/src/main/java/com/auction/kafka/dao/UserDao.java
+++ backend/kafka/kafka-streams-auction/src/main/java/com/auction/kafka/dao/UserDao.java
@@ -32,16 +32,57 @@ public class UserDao {
 
         log.info("creating new User");
         int id = -1;
-        try{
-            getSession().persist(user);
-            id = user.getUserID();
-            log.info("successfully created user with userID--"+id);
+        User ifExists = findbyEmail(user.getEmail());
+        if(ifExists != null){
+            log.info("user email already exists");
         }
-        catch(Exception ex){
-            log.error("User Registration Failed for UserDao :: ",ex);
+        else{
+            try{
+                getSession().persist(user);
+                id = user.getUserID();
+                log.info("successfully created user with userID--"+id);
+            }
+            catch(Exception ex){
+                log.error("User Registration Failed for UserDao :: ",ex);
+            }
         }
         
         return id;
+    }
+
+    @Cacheable(value = "com.auction.kafka.domain.User", key ="#result.getUserID()", unless ="#result == null")
+    public User findbyEmail(String email){
+        log.info("Find User by email:"+email+ "---UserDao");
+        
+        ///User represents the Hibernate entity not database table, same for the userID field.
+        String hqlQuery = "from User where email = :email";
+        List<User> list = null;
+        
+        try{
+            list = getSession().createQuery(hqlQuery, User.class).setParameter("email", email).list();
+        }
+        catch(Exception ex){
+            log.error("Fail Getting user by email -- UserDao", ex);
+        }
+
+        return (list!=null && list.size()>0)?list.get(0):null;
+    }
+
+    public User userLogin(String email, String password){
+        log.info("Find User :"+email+ "---UserDao");
+        
+        ///User represents the Hibernate entity not database table, same for the userID field.
+        String hqlQuery = "from User where email = :email AND userPassword = :password";
+        List<User> list = null;
+        
+        try{
+            list = getSession().createQuery(hqlQuery, User.class).setParameter("email", email).setParameter("password", password).list();
+        }
+        catch(Exception ex){
+            log.error("Fail logging user -- UserDao", ex);
+        }
+
+        return (list!=null && list.size()>0)?list.get(0):null;
     }
 
     @Cacheable(value = "com.auction.kafka.domain.User", key ="#id", unless ="#result == null")
