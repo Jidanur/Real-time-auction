7f1dc9682e7fe1ddf87eb8825d7b64e0d250cc8b
nhannpl <nplnhan157@gmail.com>
2024-04-20 16:25:17 -0500

    add cross origin in bid controller

--- backend/kafka/kafka-streams-auction/src/main/java/com/auction/kafka/controller/BidController.java
+++ backend/kafka/kafka-streams-auction/src/main/java/com/auction/kafka/controller/BidController.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping(value = "/bid")
 @Slf4j
-// @CrossOrigin()
+@CrossOrigin()
 public class BidController {
 
     @Autowired
