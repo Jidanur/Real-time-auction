1a96f2046b00aee99ddbee107592b0af8841d745
JIDAN <rahman33@myumanitoba.ca>
2024-10-02 23:18:28 -0500

    Delete frontend directory

--- frontend/src/components/ApiHelper.js
+++
@@ -1,6 +0,0 @@
-import { Client, Account } from "appwrite";
-const client = new Client();
- export const account = new Account(client);
- client
-   .setEndpoint("http://localhost/v1") // Your API Endpoint
-   .setProject("OUR_PROJECT_ID");
